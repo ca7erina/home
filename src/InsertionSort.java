c21c74ae62ca33d5baf5eb1c40af58114148d5db
xiachen <xiachen@tibco-support.com>
2015-02-03 10:05:40 +0800

    added 2 sort

---
+++ src/InsertionSort.java
@@ -0,0 +1,28 @@
+/**
+ * Created by xiaoxue on 1/26/15.
+ */
+public class InsertionSort {
+    public static void main(String args[]) {
+        int a[] = {12, 3, 8, 4, 5, 6};
+        insertionsort(a);
+        for (int i:a){
+            System.out.println(i);
+        }
+
+    }
+
+    public static void insertionsort(int[] a) {
+        for (int i = 0; i < a.length - 1; i++) {
+            for (int j = i+1; j < a.length; j++) {
+                if (a[i] > a[j]) {
+                    int temp;
+                    temp = a[i];
+                    a[i] = a[j];
+                    a[j] = temp;
+                }
+            }
+        }
+
+    }
+
+}
---
+++ src/SelectSort.java
@@ -0,0 +1,25 @@
+/**
+ * Select Sort
+ */
+public class SelectSort {
+    public static void main(String args[]) {
+        int a[] = {12, 3, 8, 4, 5, 6};
+        selectsort(a);
+        for (int i:a){
+            System.out.println(i);
+        }
+    }
+
+    public static void selectsort(int[] a) {
+        for (int i = 0; i < a.length - 1; i++) {
+            for (int j = i+1; j < a.length; j++) {
+                if (a[i] > a[j]) {
+                    int temp;
+                    temp = a[i];
+                    a[i] = a[j];
+                    a[j] = temp;
+                }
+            }
+        }
+    }
+}
