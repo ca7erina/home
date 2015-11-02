d71f1be97fcaa49ffc22cb3c5a81d78694c11212
hdit1505 <ca7erina@gmail.com>
2015-11-02 15:57:06 +0000

    update Sort

--- src/main/java/InsertionSort.java
+++ src/main/java/InsertionSort.java
@@ -1,29 +1,32 @@
 package main.java;
 
+import java.util.Arrays;
+
 /**
- * Created by xiaoxue on 1/26/15.
+ * InsertionSort: compare i with the elements before it .
  *
  */
 public class InsertionSort {
     public static void main(String args[]) {
-        int a[] = {12, 3, 8, 4, 5, 6};
+        int a[] = {12, 3, 8, 4, 5, 6,15,1,10,2};
         insertionsort(a);
-        for (int i:a){
-            System.out.println(i);
-        }
+        System.out.println(Arrays.toString(a));
 
     }
 
     public static void insertionsort(int[] a) {
-        for (int i = 0; i < a.length - 1; i++) {
-            for (int j = i+1; j < a.length; j++) {
-                if (a[i] > a[j]) {
+        for (int i = 1; i < a.length; i++) {
+            int j;
+            for (j = i; j>0; j--) {
+                if (a[j] < a[j-1]) {
                     int temp;
-                    temp = a[i];
-                    a[i] = a[j];
-                    a[j] = temp;
+                    temp = a[j];
+                    a[j] = a[j-1];
+                    a[j-1] = temp;
                 }
+
             }
+
         }
 
     }
--- src/main/java/SelectSort.java
+++ src/main/java/SelectSort.java
@@ -1,21 +1,22 @@
 package main.java;
 
+import java.util.Arrays;
+
 /**
- * Select Sort
+ * Select Sort: select the smallest on in the right and put it in the left.
  */
 public class SelectSort {
     public static void main(String args[]) {
-        int a[] = {12, 3, 8, 4, 5, 6};
+        int a[] = {12, 3, 8, 4, 5, 6, 10, 1};
         selectsort(a);
-        for (int i:a){
-            System.out.println(i);
-        }
+        System.out.println(Arrays.toString(a));
     }
 
     public static void selectsort(int[] a) {
-        for (int i = 0; i < a.length - 1; i++) {
-            for (int j = i+1; j < a.length; j++) {
-                if (a[i] > a[j]) {
+        for(int i = 0; i < a.length - 1; i++) {
+            // int smallest=a[i];
+            for(int j = i + 1; j < a.length; j++) {
+                if(a[j] < a[i]) {
                     int temp;
                     temp = a[i];
                     a[i] = a[j];
