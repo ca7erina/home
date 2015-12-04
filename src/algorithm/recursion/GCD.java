b30e462f7412500ec6cebe04b27e946c86d38a7f
ca7erina <ca7erina@gmail.com>
2015-12-04 14:57:19 +0000

    update gcd

--- src/algorithm/recursion/GCD.java
+++ src/algorithm/recursion/GCD.java
@@ -9,6 +9,17 @@ public class GCD {
     }
     return gcd(b,a%b);
   }
-  
+  /**
+   * gcd(6, 4)
+   * tryDivisor(6, 4, 4)
+  *   tryDivisor(6, 4, 3)
+   * tryDivisor(6, 4, 2)
+   * */
+  int tryDivisor(int m, int n, int g) {
+   if (((m % g) == 0) && ((n % g) == 0))
+      return g;
+   else
+      return tryDivisor(m, n, g - 1);
+}
 
 }
