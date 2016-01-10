487506d15b2dfa66b52bb2153c26560b86b4a723
hdit1505 <ca7erina@gmail.com>
2016-01-10 19:15:20 +0000

    add compoundInterest

--- src/algorithm/StackBasedOnLinkedList.java
+++ src/algorithm/StackBasedOnLinkedList.java
@@ -1,23 +1,23 @@
-package algorithm;
-
-import algorithm.linkedList.LinkedList;
-
-/**
- * Created by chenxiaoxue on 1/7/16.
- */
-public class StackBasedOnLinkedList {
-
-    private LinkedList list;
-
-    public StackBasedOnLinkedList() { // constructor
-        list = new LinkedList();
-    }
-
-    public void push(int data) { // put item on top of stack
-        list.insertHead(data);
-    }
-
-    public int pop() { // take item from top of stack
-        return list.deleteHead().data;
-    }
-}
+//package algorithm;
+//
+//import algorithm.linkedList.LinkedList;
+//
+///**
+// * Created by chenxiaoxue on 1/7/16.
+// */
+//public class StackBasedOnLinkedList {
+//
+//    private LinkedList list;
+//
+//    public StackBasedOnLinkedList() { // constructor
+//        list = new LinkedList();
+//    }
+//
+//    public void push(int data) { // put item on top of stack
+//        list.insertHead(data);
+//    }
+//
+//    public int pop() { // take item from top of stack
+//        return list.deleteHead().data;
+//    }
+//}
