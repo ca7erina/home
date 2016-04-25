9a773ef0671e93dbc53acaf54f93c38faa9dbd86
hdit1505 <ca7erina@gmail.com>
2016-04-25 11:41:31 +0100

    update

--- src/hackRank/rollDice.java
+++ src/hackRank/rollDice.java
@@ -7,101 +7,124 @@ package hackRank;
  *
  *
  */
-public class RollDice {
-    public static void main(String args[]){
-        int base =1000000;
-        int sum=0;
-        //test base times of rolling, get a six, and get avg of the number;
-        for(int i=0;i<base;i++) {
-            int got6Times = 0;
-            while(true) {
-                got6Times++;
-                if(roll() == 6) { //got 6
-                    break;
+public class rollDice {
+
+    public static void main(String[] args){
+
+        double total=0;
+        int montecarlo=100000;
+        for(int i=0;i<montecarlo;){
+            int sneeze=0;
+            int counter=0;
+            int dice=0;
+            do{
+                counter++;
+                dice = (int)(Math.random()*6)+1;
+                if((int)(Math.random()*1000)+1==1000){
+                    sneeze++;
                 }
+            }while(dice!=6);
+            if(sneeze==1){
+                total+=counter;
+                i++;
+             //   System.out.println(counter+" "+total/i);
             }
-            sum+=got6Times; //record every time get 6 took how many roll;
         }
-        System.out.println("roll dice "+base+" times, average how many rolls to get six is: " + Math.round((double)sum/base)); //avg
-
-        //get snapchat and get 6
-        base =1000000;// how many times you do this test
-        sum=0; // for calculate avg.
-        int temp = TimesOfrollAndGetSanpChat();
-        for(int i=0;i<base;i++) {
-            int got6Times = 0;
-            while(true) {
-            	got6Times++;
-                int onetime = roll() ;
-                if(onetime == 6) { //got 6
-//                	System.out.println("getsix:"+got6Times);
-//                	System.out.println("TimesOfrollAndGetSanpChat:"+temp);
-                    break;
-                } 
-            }
-            temp = TimesOfrollAndGetSanpChat();
-//            System.out.println("snapchat "+temp);
-//            System.out.println("got6Times "+got6Times);
-            if(got6Times<temp||got6Times>12){
-       		 	i--;
-            	continue;
-            }
-            //System.out.println(got6Times);
-            sum+=got6Times; //record every time get 6 took how many roll;
-        }
-        long result = Math.round((double) sum / base);
-        System.out.println("got Snapchat and got six, average how many rolls to get six: " + (result == 0 ? 0 :result)); //avg
-
-
-       //get snapchat and sneeze and get 6
-        base =1000000; //how many times you do this test
-        sum=0; // for calculate avg.
-         temp = TimesOfrollAndGetSanpChatAndGetSneez();
-         
-         for(int i=0;i<base;i++) {
-             int got6Times = 0;
-             while(true) {
-             	got6Times++;
-                 int onetime = roll() ;
-                 if(onetime == 6) { //got 6
-                 //	System.out.println("getsix:"+got6Times);
-                 //	System.out.println("TimesOfrollAndGetSanpChatAndSneeze:"+temp);
-                     break;
-                 } 
-             }
-             temp = TimesOfrollAndGetSanpChatAndGetSneez();
-             if(got6Times<temp||got6Times>18){
-        		 	i--;
-             	continue;
-             }
-             sum+=got6Times; //record every time get 6 took how many roll;
-         }
-        result = Math.round((double) sum / base);
-        System.out.println("sneezed and got Snapchat and got six ,calculate average how many rolls: " + (result == 0 ? 0 :result)); //avg
+        System.out.println(total/montecarlo);
     }
 
-
-
-
-    public static int roll(){
-        return (int)(Math.random()*6)+1; //1-6
-    }
-    
-    public static int TimesOfrollAndGetSanpChat(){
-        return 6+ (int)(Math.random()*6)+1; //6 + random(1,6)
-    }
-    
-    public static int TimesOfrollAndGetSanpChatAndGetSneez(){
-    	  return 6+ (int)(Math.random()*6)+1+(int)(Math.random()*6)+1;//6 + random(1,6) + random(1,6)
-    }
-
-    public static long timeGettingSnapchat(){
-        return (long)(Math.random()*2); //this time can roll6 times;
-    }
-
-    public static long timeGettingSneeze(){
-        return (long)(Math.random()*2); //< half mins
-    }
+//    public static void main(String args[]){
+//        int base =1000000;
+//        int sum=0;
+//        //test base times of rolling, get a six, and get avg of the number;
+//        for(int i=0;i<base;i++) {
+//            int got6Times = 0;
+//            while(true) {
+//                got6Times++;
+//                if(roll() == 6) { //got 6
+//                    break;
+//                }
+//            }
+//            sum+=got6Times; //record every time get 6 took how many roll;
+//        }
+//        System.out.println("roll dice "+base+" times, average how many rolls to get six is: " + Math.round((double)sum/base)); //avg
+//
+//        //get snapchat and get 6
+//        base =1000000;// how many times you do this test
+//        sum=0; // for calculate avg.
+//        int temp = TimesOfrollAndGetSanpChat();
+//        for(int i=0;i<base;i++) {
+//            int got6Times = 0;
+//            while(true) {
+//            	got6Times++;
+//                int onetime = roll() ;
+//                if(onetime == 6) { //got 6
+////                	System.out.println("getsix:"+got6Times);
+////                	System.out.println("TimesOfrollAndGetSanpChat:"+temp);
+//                    break;
+//                }
+//            }
+//            temp = TimesOfrollAndGetSanpChat();
+////            System.out.println("snapchat "+temp);
+////            System.out.println("got6Times "+got6Times);
+//            if(got6Times<temp||got6Times>12){
+//       		 	i--;
+//            	continue;
+//            }
+//            //System.out.println(got6Times);
+//            sum+=got6Times; //record every time get 6 took how many roll;
+//        }
+//        long result = Math.round((double) sum / base);
+//        System.out.println("got Snapchat and got six, average how many rolls to get six: " + (result == 0 ? 0 :result)); //avg
+//
+//
+//       //get snapchat and sneeze and get 6
+//        base =1000000; //how many times you do this test
+//        sum=0; // for calculate avg.
+//         temp = TimesOfrollAndGetSanpChatAndGetSneez();
+//
+//         for(int i=0;i<base;i++) {
+//             int got6Times = 0;
+//             while(true) {
+//             	got6Times++;
+//                 int onetime = roll() ;
+//                 if(onetime == 6) { //got 6
+//                 //	System.out.println("getsix:"+got6Times);
+//                 //	System.out.println("TimesOfrollAndGetSanpChatAndSneeze:"+temp);
+//                     break;
+//                 }
+//             }
+//             temp = TimesOfrollAndGetSanpChatAndGetSneez();
+//             if(got6Times<temp||got6Times>18){
+//        		 	i--;
+//             	continue;
+//             }
+//             sum+=got6Times; //record every time get 6 took how many roll;
+//         }
+//        result = Math.round((double) sum / base);
+//        System.out.println("sneezed and got Snapchat and got six ,calculate average how many rolls: " + (result == 0 ? 0 :result)); //avg
+//    }
+//
+//
+//    public static int roll(){
+//        return (int)(Math.random()*6)+1; //1-6
+//    }
+//
+//    public static int TimesOfrollAndGetSanpChat(){
+//        return 6+ (int)(Math.random()*6)+1; //6 + random(1,6)
+//    }
+//
+//    public static int TimesOfrollAndGetSanpChatAndGetSneez(){
+//    	  return 6+ (int)(Math.random()*6)+1+(int)(Math.random()*6)+1;//6 + random(1,6) + random(1,6)
+//    }
+//
+//    public static long timeGettingSnapchat(){
+//        return (long)(Math.random()*2); //this time can roll6 times;
+//    }
+//
+//    public static long timeGettingSneeze(){
+//        return (long)(Math.random()*2); //< half mins
+//    }
 
 
 }
